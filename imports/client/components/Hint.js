9e3c4a04b7aa6ad17abc1f262a3e39fbd493ebb5
david hurley <davidhurley@davids-MacBook-Air.local>
2016-07-12 20:48:10 -0400

    fix up

--- imports/client/components/Hint.js
+++ imports/client/components/Hint.js
@@ -5,16 +5,16 @@ const Hint = React.createClass({
 	render() {
 		const question = this.props.question;
 		const width = parseFloat(question.seconds / question.totalTime * 100);
-		const width2 = parseFloat(this.props.question.right / this.props.question.winningThreshold * 100);
+		const width2 = 100 - parseFloat(this.props.question.right / this.props.question.winningThreshold * 100);
 		return (
 			<div>
+				<ProgressBar width={width2} />
 				<ProgressBar width={width} />
 				<div className='container loser'>
 					{ question.guess != "" ? <div className='text'>You guessed { question.guess } :(</div> : null }
 					<h1>{question.num1} {question.operator} {question.num2} = {question.answer }</h1> 
 					<button className='submit' onClick={this.props.continueEvaluation.bind(this)}>continue</button>
 				</div>
-				<ProgressBar style='bottomFixed' width={width2} />
 			</div>
 		)
 	}
